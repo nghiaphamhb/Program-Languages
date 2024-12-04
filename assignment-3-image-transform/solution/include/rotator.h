b56017e845e46ed29a3ef907e2b8f021aedb4ae9
NghiaPhamHB <phamdangtrungnghia@gmail.com>
2024-12-05 02:24:49 +0300

    updated 5/12

--- assignment-3-image-transform/solution/include/rotator.h
+++ assignment-3-image-transform/solution/include/rotator.h
@@ -7,8 +7,8 @@
 #include "image.h"
 
 struct image* rotate( struct image* source);
-// struct image* rotated_right_90_img ( struct image* source);
+struct image* rotated_right_90_img ( struct image* source);
 struct image* rotated_left_90_img ( struct image* source);
-// struct image* flipped_vertically_img( struct image* source);
-// struct image* flipped_horizontally_img( struct image* source);
+struct image* flipped_vertically_img( struct image* source);
+struct image* flipped_horizontally_img( struct image* source);
 #endif
